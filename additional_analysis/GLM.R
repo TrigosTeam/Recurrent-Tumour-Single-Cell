5c2a816a0ec29c657aa86403d52e200fa650e48b
CathyXD <Sirui.Weng@petermac.org>
2026-03-16 13:02:38 +1100

    minor edits

--- additional_analysis/GLM.R
+++ additional_analysis/GLM.R
@@ -111,222 +111,12 @@ df_expanded <- df_expanded %>%
 
 
 celltypes <- unique(meta$cell_anno)
-
-test_model <- lapply(celltypes, function(anno){
-  cat(anno, "\n")
-  fit1 <- glmmTMB(
-    cbind(n, total_normal - n) ~ site + pathology + (1 | patient),
-    data = df %>% filter(cell_anno == anno),
-    family = betabinomial()
-  )
-  fit2 <- glmmTMB(
-    cbind(n, total_normal - n) ~ pathology + patient +  (1 | site),
-    data = df %>% filter(cell_anno == anno),
-    family = betabinomial()
-  )
-  fit3 <- glmmTMB(
-    cbind(n, total_normal - n) ~ pathology + (1 | patient/site),
-    data = df %>% filter(cell_anno == anno),
-    family = betabinomial()
-  )
-  fit4 <- glmmTMB(
-    cbind(n, total_normal - n) ~ pathology,
-    data = df %>% filter(cell_anno == anno),
-    family = betabinomial()
-  )
-  fit5 <- glmmTMB(
-    cbind(n, total_normal - n) ~ pathology+ (1|patient),
-    data = df %>% filter(cell_anno == anno),
-    family = betabinomial()
-  )
-  fit6 <- glmmTMB(
-    cbind(n, total_normal - n) ~ pathology+site,
-    data = df %>% filter(cell_anno == anno),
-    family = betabinomial()
-  )
-  fit7<- glmmTMB(
-    cbind(n, total_normal - n) ~ pathology+site +patient,
-    data = df %>% filter(cell_anno == anno),
-    family = betabinomial()
-  )
-  
-  fit8 <- glmmTMB(
-    cbind(n, total_normal - n) ~ site,
-    data = df %>% filter(cell_anno == anno),
-    family = betabinomial()
-  )
-
-  fit <- glmmTMB(
-    cbind(n, total_normal - n) ~  pathology + (1 | site),
-    data = df %>% filter(cell_anno == anno),
-    family = betabinomial()
-  )
-  
-  fit9 <- glmmTMB(
-    cbind(n, total_normal - n) ~ pathology + patient,
-    data = df %>% filter(cell_anno == anno),
-    family = betabinomial()
-  )
-  
-  fixed_only          = fit4 
-  fixed_site          = fit6
-  fixed_patient       = fit9
-  fixed_both          = fit7
-  site_only = fit8
-  re_site             = fit
-  re_patient          = fit5
-  re_patient_site     = fit3
-  re_site_fix_patient = fit2
-  re_patient_fix_site = fit1
-  print(anova(site_only,fixed_only,fixed_site,fixed_patient,fixed_both,re_site,re_patient,re_patient_site,re_site_fix_patient,re_patient_fix_site))
-  return(anova(site_only,fixed_only,fixed_site,fixed_patient,fixed_both,re_site,re_patient,re_patient_site,re_site_fix_patient,re_patient_fix_site))
-})
-
-### centered log-ratio transform proportion and use distance===========
-set.seed(123)
-celltypes <- unique(df$cell_anno)
-
-# Metadata
-sample_meta <- df  %>%
-  dplyr::select(sample, patient, pathology, site, total_normal) %>%
-  distinct()
-
-
 counts <- reshape2::dcast(data = df %>% dplyr::select(sample, cell_anno, n), sample~ cell_anno, fill = 0 )
 rownames(counts) <- counts$sample
 counts <- counts[, -1]
 counts <- counts[sample_meta$sample, ]
 # Proportions (compositional data)
 props <- counts / rowSums(counts)
-
-#========== PERMANOVA ==========#
-
-# Aitchison distance (recommended for compositional data)
-# Replace zeros first with a small pseudocount
-counts_nz <- counts + 0.5
-clr_mat   <- as.matrix(clr(counts_nz))   # centered log-ratio transform
-
-aitchison_dist <- dist(clr_mat, method = "euclidean")  # Euclidean on CLR = Aitchison
-
-perm_patient_strata <- adonis2(
-  aitchison_dist ~ pathology + site ,
-  data         = sample_meta,
-  strata       = sample_meta$patient,
-  permutations = 999,
-  by           = "margin"
-)
-
-print(perm_patient_strata)
-# Permutation test for adonis under reduced model
-# Marginal effects of terms
-# Blocks:  strata 
-# Permutation: free
-# Number of permutations: 999
-# 
-# adonis2(formula = aitchison_dist ~ pathology + site, data = sample_meta, permutations = 999, by = "margin", strata = sample_meta$patient)
-# Df SumOfSqs      R2      F Pr(>F)    
-# pathology  2    57.44 0.06891 1.6798  0.044 *  
-#   site       8   360.15 0.43207 2.6331  0.001 ***
-#   Residual  22   376.14 0.45125                  
-# Total     32   833.54 1.00000                  
-# ---
-#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-#========== BETADISPER (Homogeneity of Dispersion) ==========#
-
-bd_disease <- betadisper(aitchison_dist, sample_meta$pathology)
-print(permutest(bd_disease, permutations = 999))
-# 
-# Permutation test for homogeneity of multivariate dispersions
-# Permutation: free
-# Number of permutations: 999
-# 
-# Response: Distances
-# Df Sum Sq Mean Sq      F N.Perm Pr(>F)
-# Groups     2  0.902 0.45118 0.1468    999  0.856
-# Residuals 30 92.216 3.07386  
-
-bd_organ <- betadisper(aitchison_dist, sample_meta$site)
-print(permutest(bd_organ, permutations = 999))
-# Permutation test for homogeneity of multivariate dispersions
-# Permutation: free
-# Number of permutations: 999
-# 
-# Response: Distances
-# Df Sum Sq Mean Sq      F N.Perm Pr(>F)  
-# Groups     8 43.879  5.4849 2.6415    999  0.018 *
-#   Residuals 24 49.834  2.0764                       
-# ---
-#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-plot(bd_organ, main = "Dispersion by metastatic site")
-
-
-cat("\n========== Linear Mixed Models (per cell type) ==========\n")
-
-clr_df <- as.data.frame(clr_mat)
-clr_df <- cbind(clr_df, sample_meta)
-
-lmm_results <- lapply(celltypes, function(ct) {
-  formula_str <- paste0("`", ct, "` ~  site + (1 | patient)")
-  fit <- lmer(as.formula(formula_str), data = clr_df, REML = FALSE)
-  
-  # Type III ANOVA-style F-tests
-  an  <- anova(fit)
-  data.frame(
-    cell_type     = ct,
-    term          = rownames(an),
-    F_value       = an$`F value`,
-    p_value       = an$`Pr(>F)`
-  )
-})
-
-lmm_df <- do.call(rbind, lmm_results)
-
-# FDR correction separately for each term
-lmm_df <- lmm_df %>%
-  group_by(term) %>%
-  mutate(p_adj_BH = p.adjust(p_value, method = "BH")) %>%
-  ungroup()
-
-print(lmm_df %>% filter(p_adj_BH < 0.05) %>% arrange(term, p_adj_BH))
-
-
-sig_celltypes <- lmm_df %>%
-  filter(term == "site", p_adj_BH < 0.05) %>%
-  pull(cell_type)
-
-# Run emmeans contrasts for each significant cell type
-posthoc_results <- lapply(celltypes, function(ct) {
-  
-  formula_str <- paste0("`", ct, "` ~ site + (1 | patient)")
-  fit <- lmer(as.formula(formula_str), data = clr_df, REML = T)
-  
-  # Estimated marginal means per site
-  emm  <- emmeans(fit, ~ site)
-  
-  # Each site vs grand mean (enrichment/depletion)
-  cont <- contrast(emm, method = "eff") %>%
-    as.data.frame() %>%
-    mutate(
-      cell_type = ct,
-      direction = ifelse(estimate > 0, "Enriched", "Depleted")
-    )
-  cont
-})
-
-posthoc_df <- do.call(rbind, posthoc_results)
-
-# FDR correction ACROSS all cell types and sites together
-posthoc_df$p_adj_BH <- p.adjust(posthoc_df$p.value, method = "BH")
-
-# Significant enrichments
-sig_enrichments <- posthoc_df %>%
-  filter( p.value < 0.05) %>%
-  arrange(cell_type,  p.value)
-
-print(sig_enrichments)
-write.table(posthoc_df, "GLM_emm_result_celltype_site.csv", quote = F, row.names = F, sep = ",")
-
 props_long <- as.data.frame(props) %>%
   mutate(sample_id = sample_meta$sample_id,
          pathology = sample_meta$pathology,
@@ -351,42 +141,125 @@ p_bar_disease <- props_long %>%
 p_bar_disease
 ggsave("celltype_bypathology.pdf", width = 8, height = 4)
 
+library(speckle)
+library(limma)
+library(ggplot2)
+
+final_normalsrt <- readRDS("~/CASCADEpaper/paper/normal_cells_202406/subtype/final_normalsrt.Rds")
+final_normalsrt$cell_type <- ifelse(final_normalsrt$cell_anno%in% c("T cells","B cells","Plasma cells",  "Macrophages"), "immune cells", 
+                                    ifelse(final_normalsrt$cell_anno%in% c("Fibroblasts","Adipocytes","Pericytes", "Endothelial cells"), "stromal cells", "others"))
+final_normalsrt$pathology <- ifelse(final_normalsrt$patient %in% c("CA0090", "CA0046"), "NE", ifelse(final_normalsrt$patient %in% c("CA0027", "CA0058"), "Mixed", "AD"))
+
+props <- getTransformedProps(clusters = final_normalsrt$cell_anno,sample = final_normalsrt$sample, transform="logit")
+propeller(x = final_normalsrt, clusters = final_normalsrt$cell_anno,sample = final_normalsrt$sample, group = final_normalsrt$site,  transform="logit")
+my_table <- props$TransformedProps
+propmat <- matrix(my_table, 
+                  nrow = nrow(my_table), 
+                  ncol = ncol(my_table),
+                  dimnames = dimnames(my_table))
+
+
+patient <- substr(colnames(props$Proportions), 1, 6)
+sites <- colnames(props$Proportions)
+sites[grep("brain", sites)] <- "brain"
+sites[grep("dura", sites)] <- "dura"
+sites[grep("prostate", sites)] <- "prostate"
+sites[grep("liver", sites)] <- "liver"
+sites[grep("fat", sites)] <- "fat"
+sites[grep("lymph|hilar", sites)] <- "LN"
+sites[grep("rib|vertebra", sites)] <- "bone"
+sites[grep("lung", sites)] <- "lung"
+sites[grep("abdomen", sites)] <- "abdomen"
+sites[grep("bladder", sites)] <- "bladder"
+pathology <-  ifelse(patient %in% c("CA0090", "CA0046"), "NE", ifelse(patient %in% c("CA0027", "CA0058"), "Mixed", "AD"))
+
+
+design <- model.matrix(~patient)
+fit0 <- lmFit(props$TransformedProps, design=design)
+fit0 <- eBayes(fit0)
+
+
+design <- model.matrix(~ sites+ patient)
+fit1<- lmFit(props$TransformedProps, design=design)
+fit1 <- eBayes(fit1)
+
+
+des.tech <- model.matrix(~sites)
+dupcor <- duplicateCorrelation(props$TransformedProps, design=des.tech,
+                               block=patient)
+dupcor
+fit1d <- lmFit(props$TransformedProps, design=des.tech, block=patient, 
+               correlation=dupcor$consensus)
+fit1d <- eBayes(fit1d)
+
+
+design <- model.matrix(~patient + sites + pathology )
+fit2<- lmFit(props$TransformedProps, design=design)
+fit2 <- eBayes(fit2)
+
+# Coefficients not estimable: pathologyMixed pathologyNE 
+# Warning: Partial NA coefficients for 14 probe(s)
+
+des.tech2 <- model.matrix(~ sites + pathology)
+
+dupcor <- duplicateCorrelation(props$TransformedProps, design=des.tech2,
+                               block=patient)
+dupcor
+fit2d<- lmFit(props$TransformedProps, design=des.tech2, block=patient, 
+              correlation=dupcor$consensus)
+fit2d<- eBayes(fit2d)
+
+aic_model <- function(fit, n) {
+  k <- ncol(fit$design)          # number of parameters
+  rss <- fit$sigma^2 * fit$df.residual
+  aic <- n * log(rss / n) + 2 * k
+  return(aic)
+}
+
+compare_model <- sapply(list(fit0, fit1, fit1d, fit2, fit2d), function(fit){
+  data.frame(median_sigma = median(fit$sigma), 
+             mean_AIC = mean(aic_model(fit, 33)))
+})
+colnames(compare_model) <- c("~ patient", "~ patient + sites", "~ sites + block(patient)", "~ patient + sites + pathology", "~ patient + sites + block(pathology)")
+compare_model
+
+# best fitting model ~ 0 + patient + sites
+write.table(topTable(fit1, coef= 2:9, number = Inf), "limma_celltype_site.csv", sep = "," ) 
+
+
 
 ## Compare Neuron cells between patients -----------
-ct = "Neurons"
-formula_str <- paste0("`", ct, "` ~ patient + (1 | site)")
-fit <- lmer(as.formula(formula_str), data = clr_df, REML = TRUE)
-
-# Estimated marginal means per patient 
-emm  <- emmeans(fit, ~ patient)
-
-# Each patient vs grand mean (enrichment/depletion)
-cont <- contrast(emm, method = "eff") %>%
-  as.data.frame() %>%
-  mutate(
-    cell_type = ct,
-    direction = ifelse(estimate > 0, "Enriched", "Depleted")
-  )
-cont
-
-
-ct = "Macrophages"
-formula_str <- paste0("`", ct, "` ~ patient + (1 | site)")
-fit <- lmer(as.formula(formula_str), data = clr_df, REML = TRUE)
-
-# Estimated marginal means per patient 
-emm  <- emmeans(fit, ~ patient)
-
-# Each patient vs grand mean (enrichment/depletion)
-cont <- contrast(emm, method = "eff") %>%
-  as.data.frame() %>%
-  mutate(
-    cell_type = ct,
-    direction = ifelse(estimate > 0, "Enriched", "Depleted")
-  )
-cont %>% filter(p.value <0.05)
-
-# Compare archetype signature expression variance between subclones
+design <- model.matrix(~ 0+ patient)
+cont.matrix <- makeContrasts(
+  CA90_vs_CA27 = patientCA0090 - patientCA0034,
+  CA90_vs_CA34 = patientCA0090 - patientCA0034,
+  CA90_vs_CA35 = patientCA0090 - patientCA0035,
+  CA90_vs_CA43 = patientCA0090 - patientCA0043,
+  CA90_vs_CA46 = patientCA0090 - patientCA0046,
+  CA90_vs_CA58 = patientCA0090 - patientCA0058,
+  CA90_vs_CA76 = patientCA0090 - patientCA0076,
+  CA90_vs_CA83 = patientCA0090 - patientCA0083,
+  levels = design
+)
+fitp <- lmFit(props$TransformedProps, design=design)
+fitp <- contrasts.fit(fitp, cont.matrix)
+topTable(eBayes(fitp), number = Inf)
+# CA90_vs_CA27 CA90_vs_CA34 CA90_vs_CA35 CA90_vs_CA43 CA90_vs_CA46 CA90_vs_CA58 CA90_vs_CA76 CA90_vs_CA83   AveExpr         F     P.Value  adj.P.Val
+# Endothelial cells             -2.5199156   -2.5199156  -1.73908572   -2.6270029   -2.9575086   -3.3209973  -2.00663270  -0.63396677 -2.201612 3.5111344 0.005495571 0.04898542
+# Neurons                        3.8987880    3.8987880   3.21590340    4.0314680    3.0307559    3.4643485   4.90614646   3.48011916 -5.938957 3.3719327 0.006997917 0.04898542
+# Pericytes                     -1.2738429   -1.2738429  -1.39604541   -1.6497529   -2.2873180   -0.2361942  -0.25524445   0.26768844 -3.399101 2.4955119 0.033267857 0.15525000
+# Fibroblasts                   -1.7544596   -1.7544596  -1.61174734   -1.0486089   -0.9350013    0.6009924  -0.18498027  -0.31619621 -1.637859 1.7244610 0.133426276 0.38333635
+# T cells                        0.5557080    0.5557080  -0.63264510    0.0359266    1.2142092    1.4797940   0.09761292  -0.97024369 -2.253133 1.7099374 0.136905839 0.38333635
+# Lymphatic endothelial cells   -0.4024341   -0.4024341   0.95605414    0.1700341   -0.4240143    1.1055200   1.74331705   1.06129677 -4.985943 1.3837625 0.241258689 0.56293694
+# Adipocytes                     0.6044301    0.6044301  -0.50302759    1.7753021    0.6043327    1.4151116   1.30152085   1.02053036 -4.659972 1.0338552 0.424640966 0.77593417
+# Macrophages                   -1.0830686   -1.0830686   0.04459526   -0.9538206   -0.9832777   -1.4334289  -0.66701718  -1.26354296 -1.618441 0.9669673 0.469278309 0.77593417
+# Chondrocytes                  -0.1896550   -0.1896550  -0.69016457    0.3103482   -0.5096072   -0.5408318  -2.05914297  -0.11270080 -5.814471 0.8729933 0.536762653 0.77593417
+# Plasma cells                   1.5574820    1.5574820  -0.08408156    0.0744516    0.4052557    1.1230425   0.22497801  -0.18014676 -5.127399 0.7979835 0.594003624 0.77593417
+# EndoMT                        -1.2595707   -1.2595707  -1.66813152   -1.0347518   -1.0056734   -1.4234303  -0.56502062  -0.04034088 -4.907418 0.7779829 0.609662559 0.77593417
+# B cells                        1.4131165    1.4131165   0.71019027    1.2623198    0.7446202    2.0150578  -0.26372095   0.41581555 -4.435993 0.7081994 0.665170791 0.77603259
+# Hepotocytes                   -1.0465846   -1.0465846   0.08327947   -1.3312275   -0.1018680   -1.4702409  -0.72198448  -1.07968347 -5.482341 0.4032363 0.894139039 0.92294200
+# Epithelial cells               0.0611833    0.0611833   1.60696835    0.8842529    0.9814196    0.3800160   0.09944445   0.37464922 -4.842821 0.3536568 0.922941997 0.92294200
+# Compare archetype signature expression variance between subclones =====================
 subclones <- readRDS("~/CASCADEpaper/paper/Fig3_genomics/ATAClone/subclones.Rds")
 paths <- system(paste0("realpath ~/CASCADEpaper/paper/Fig5_archetype/4_srt_meta/*.Rds"), intern = T)
 names(paths) <- gsub(".Rds", "", sapply(strsplit(paths, split = "/"), '[', 8))
